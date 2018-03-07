c078b8b72ebecb84adb6a8f1f684839eec344135
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-08 00:00:13 +0100

    Moved instruction functions to their own files in srcs/op_func/*.c .

--- vm/srcs/op_func/op_live.c
+++ vm/srcs/op_func/op_live.c
@@ -1,20 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   actions_1.c                                        :+:      :+:    :+:   */
+/*   op_live.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/06 17:53:08 by mgonon            #+#    #+#             */
-/*   Updated: 2018/03/06 22:51:56 by mgonon           ###   ########.fr       */
+/*   Created: 2018/03/07 23:55:03 by gudemare          #+#    #+#             */
+/*   Updated: 2018/03/07 23:55:17 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
-
-/*
-** List of actions that can be performed by the process
-*/
 
 void	do_live(t_process *process, t_env *env)
 {
@@ -22,29 +18,5 @@ void	do_live(t_process *process, t_env *env)
 	env->nb_live++;
 	if (env->nb_live == NBR_LIVE && process_are_alive(env))
 		env->cycle_to_die -= CYCLE_DELTA;
-	(void)env;
-}
-
-void	do_ld(t_process *process, t_env *env)
-{
-	debug_actions(process, "ld");
-	(void)env;
-}
-
-void	do_st(t_process *process, t_env *env)
-{
-	debug_actions(process, "st");
-	(void)env;
-}
-
-void	do_zjmp(t_process *process, t_env *env)
-{
-	debug_actions(process, "zjmp");
-	(void)env;
-}
-
-void	do_ldi(t_process *process, t_env *env)
-{
-	debug_actions(process, "ldi");
 	(void)env;
 }
--- vm/srcs/op_func/op_zjmp.c
+++ vm/srcs/op_func/op_zjmp.c
@@ -1,23 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   actions_4.c                                        :+:      :+:    :+:   */
+/*   op_zjmp.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/06 18:31:07 by mgonon            #+#    #+#             */
-/*   Updated: 2018/03/06 19:46:19 by mgonon           ###   ########.fr       */
+/*   Created: 2018/03/07 23:56:02 by gudemare          #+#    #+#             */
+/*   Updated: 2018/03/07 23:56:10 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-/*
-** List of actions that can be performed by the process
-*/
-
-void	do_aff(t_process *process, t_env *env)
+void	do_zjmp(t_process *process, t_env *env)
 {
-	debug_actions(process, "aff");
+	debug_actions(process, "zjmp");
 	(void)env;
 }
