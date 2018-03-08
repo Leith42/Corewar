04f26d10d8eb37a133978ef8c04ca1f3ce8183bc
Mathieu GONON <mgonon@student.42.fr>
2018-03-08 19:27:41 +0100

    delete false rule in live (epitech subject)

--- vm/srcs/op_func/op_live.c
+++ vm/srcs/op_func/op_live.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_live.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:55:03 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/07 23:55:17 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/08 19:26:56 by mgonon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,5 @@ void	do_live(t_process *process, t_env *env)
 {
 	debug_actions(process, "live");
 	env->nb_live++;
-	if (env->nb_live == NBR_LIVE && process_are_alive(env))
-		env->cycle_to_die -= CYCLE_DELTA;
 	(void)env;
 }
