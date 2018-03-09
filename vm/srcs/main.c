6c4c3d7eb0ec06f7f3fc6cf4d0686ee0cd40b86c
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-09 21:42:48 +0100

    Added check to see if an int is four bytes at runtime.

--- vm/srcs/main.c
+++ vm/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 16:21:39 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/09 01:57:08 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/09 21:41:53 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ int			main(int argc, char *argv[])
 	{
 		ft_putendl("Usage: ./corewar \
 [-dump nbr_cycles] [[-n number] champion1.cor] ...");
+	}
+	else if (sizeof(int) < 4)
+	{
+		ft_dprintf(2, "ERROR : int size is less than 4 bytes on this machine. \
+Aborting.\n");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
