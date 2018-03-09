2b25cfe78938b64ab5fb7f70e61f71d2f14d029d
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-09 01:25:56 +0100

    Remade kill_dead_process() entirely. Now works flawlessly.

--- vm/srcs/run/process.c
+++ vm/srcs/run/process.c
@@ -6,42 +6,12 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 22:47:11 by mgonon            #+#    #+#             */
-/*   Updated: 2018/03/08 23:52:41 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/09 01:24:51 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 #include <stdlib.h>
-
-size_t	kill_dead_process(t_env *env)
-{
-	t_list	*lst_cur;
-	t_list	*lst_prev;
-	t_list	**start;
-	size_t	nb_proc_killed;
-
-	nb_proc_killed = 0;
-	start = &(env->process);
-	lst_prev = env->process;
-	lst_cur = env->process;
-	while (lst_cur)
-	{
-		if (!((t_process *)lst_cur->content)->is_alive && nb_proc_killed++)
-		{
-			if (lst_cur == *start)
-				*start = lst_cur->next;
-			else
-				lst_prev->next = lst_cur->next;
-			free(lst_cur);
-		}
-		else
-			((t_process *)lst_cur->content)->is_alive = 0;
-		if (!(lst_prev = lst_prev->next))
-			break ;
-		lst_cur = lst_prev->next;
-	}
-	return (nb_proc_killed);
-}
 
 void	add_new_process(t_env *env, unsigned int champion_id)
 {
@@ -59,4 +29,31 @@ void	add_new_process(t_env *env, unsigned int champion_id)
 	if (!(tmp = ft_lstnew((void *)process, sizeof(t_process))))
 		ft_free_exit(*env, NULL, 1, 0);
 	ft_lstpush_front(&(env->process), tmp);
+}
+
+size_t	kill_dead_process(t_env *env)
+{
+	size_t	nb_death;
+	t_list	**prev_next;
+	t_list	*cur;
+
+	nb_death = 0;
+	prev_next = &env->process;
+	cur = env->process;
+	while (cur)
+	{
+		if (((t_process *)(cur->content))->is_alive == false)
+		{
+			*prev_next = cur->next;
+			free(cur->content);
+			free(cur);
+			nb_death++;
+			cur = *prev_next;
+			continue;
+		}
+		((t_process *)(cur->content))->is_alive = false;
+		prev_next = &(cur->next);
+		cur = cur->next;
+	}
+	return (nb_death);
 }
