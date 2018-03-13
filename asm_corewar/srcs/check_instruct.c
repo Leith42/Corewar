bfeedc8a63b11c912fb2809e7ccaeee13c40366f
lmartin- <lmartin-@student.42.fr>
2018-03-13 20:26:08 +0100

    check_params create

--- asm_corewar/srcs/check_instruct.c
+++ asm_corewar/srcs/check_instruct.c
@@ -12,20 +12,53 @@
 
 #include "asm.h"
 
-int			get_inst(char **inst)
+int					check_params(char **inst, int opcode)
 {
-	int n;
-	unsigned char
+	int		n;
+	int		type;
 
-	n = 0;
+	n = -1;
 	while (inst[++n])
 	{
-		if (inst[n][ft_strlen(inst[n]) - 1] == ':') //Label en debut de chaine
-		 	n++;
+		if (g_op_tab[opcode].param_nb <= n)
+			return (0);
+		if (inst[n][0] == 'r')
+			type = T_REG;
+		if (inst[n][0] == '%')
+			type = T_DIR;
+		else
+			type = T_IND;
+		if (!(type & g_op_tab[opcode].param_type[n]))
+			return (0);
+	}
+	return (1);
+}
+
+int					get_inst(char **inst)
+{
+	int 			n;
+	int				i;
+	int 			oc;
+	unsigned char	op[12];
+
+	i = 0;
+	n = (inst[0][ft_strlen(inst[0]) - 1] == ':') ? 1 : 0;
+	/*
+	** Le cas du label en debut de chaine n'est pas gere
+	*/
+	while (i <= 16 && (ft_strcmp(g_op_tab[i].name, inst[n]) != 0))
+		i++;
+	if (i == 16)
+		return (0); //Erreur instruct
+	op[n++] = i + 1;
+	check_params(inst + n, i);
+	if (g_op_tab.opc == TRUE)
+	{
+
 	}
 }
 
-int			check_inst(char *line, unsigned char **tab)
+int					check_inst(char *line, unsigned char **tab)
 {
 	char	*line;
 	char	**inst;
