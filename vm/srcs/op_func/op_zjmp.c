69450717f310316f5cc51d674ddef79f468bd342
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-13 21:17:26 +0100

    Corrected op_zjmpc().

--- vm/srcs/op_func/op_zjmp.c
+++ vm/srcs/op_func/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:56:02 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/13 20:52:54 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/13 21:16:44 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 int	do_zjmp(t_process *process, t_env *env)
 {
 	if (process->carry == 1)
-	{
-		process->pc += (get_param_raw_value(env, process->pc + 1,
-					T_DIR, 0x09) % IDX_MOD);
-	}
+		process->pc += (get_param_raw_value(env, process->pc + 1, T_DIR, 0x09));
 	else
 		process->pc += 1 + 2;
 	process->pc %= MEM_SIZE;
