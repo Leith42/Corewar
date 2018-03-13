9677d84f1e8f64da2b913b04ea290abf38a5cb5b
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-14 00:14:15 +0100

    Simplified op_add() and op_sub(). Used enums in op_zjmp() for sanity.

--- vm/srcs/op_func/op_add.c
+++ vm/srcs/op_func/op_add.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:51:00 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/13 23:37:53 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/14 00:12:13 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,15 @@ int	do_add(t_process *process, t_env *env)
 	unsigned int	res;
 
 	tmp = 0;
-	while (tmp < 3 && get_param_type(env, process->pc, 0x04, tmp) == T_REG)
+	while (tmp < 3 && get_param_type(env, process->pc, OP_ADD, tmp) == T_REG)
 		tmp++;
-	res = 0;
-	if (tmp == 3)
-	{
-		a = get_param_raw_value(env, process->pc + 2, T_REG, 0x04);
-		b = get_param_raw_value(env, process->pc + 3, T_REG, 0x04);
-		c = get_param_raw_value(env, process->pc + 4, T_REG, 0x04);
-		if (a < 1 || a > 16 || b < 1 || b > 16 || c < 1 || c > 16)
-			return (-1);
-		res = process->reg[a] + process->reg[b];
-		process->reg[c] = res;
-	}
+	a = get_param_raw_value(env, process->pc + 2, T_REG, OP_ADD);
+	b = get_param_raw_value(env, process->pc + 3, T_REG, OP_ADD);
+	c = get_param_raw_value(env, process->pc + 4, T_REG, OP_ADD);
+	if (tmp < 3 || a < 1 || a > 16 || b < 1 || b > 16 || c < 1 || c > 16)
+		return (-1);
+	res = process->reg[a] + process->reg[b];
+	process->reg[c] = res;
 	process->pc += 1 + 1 + 1 + 1;
 	return ((int)res);
 }
--- vm/srcs/op_func/op_zjmp.c
+++ vm/srcs/op_func/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:56:02 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/13 23:07:45 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/14 00:09:58 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ int	do_zjmp(t_process *process, t_env *env)
 {
 	if (process->carry == 1)
 		process->pc += (((short)get_param_raw_value(env, process->pc + 1,
-						T_DIR, 0x09)) % IDX_MOD);
+			T_DIR, OP_ZJMP)) % IDX_MOD);
 	else
 		process->pc += 1 + 2;
-	process->pc %= MEM_SIZE;
 	return (0);
 }
