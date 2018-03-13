81aa575e7794537566644ac53a841d764e3f6fe8
Mathieu GONON <mgonon@student.42.fr>
2018-03-14 00:36:43 +0100

    op_fork not tested yet

--- vm/srcs/op_func/op_fork.c
+++ vm/srcs/op_func/op_fork.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:53:50 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/12 22:10:53 by mgonon           ###   ########.fr       */
+/*   Updated: 2018/03/14 00:35:49 by mgonon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	do_fork(t_process *process, t_env *env)
 {
-	(void)process;
-	(void)env;
+	short			param;
+	t_process		*fork;
+
+	param = (short)get_param_raw_value(env, process->pc + 1, T_DIR, OP_FORK);
+	fork = add_new_process(env, process->champ_id);
+	memcpy(fork, process, sizeof(t_process));
+	fork->pc = process->pc + (param % IDX_MOD);
 	return (0);
 }
