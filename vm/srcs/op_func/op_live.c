85b26111cd34b72a0388ce622bb0fbea69d3965e
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-13 19:58:13 +0100

    op_live now sets PC with modulo MEM_SIZE. Added comment reminding to render this process more generic.

--- vm/srcs/op_func/op_live.c
+++ vm/srcs/op_func/op_live.c
@@ -6,11 +6,15 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:55:03 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/12 22:11:06 by mgonon           ###   ########.fr       */
+/*   Updated: 2018/03/13 19:56:41 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+/*
+** The advancement of the PC is to be made more generic.
+*/
 
 int		do_live(t_process *process, t_env *env)
 {
@@ -26,6 +30,7 @@ int		do_live(t_process *process, t_env *env)
 	}
 	process->is_alive = true;
 	process->pc += 4 + 1;
+	process->pc %= MEM_SIZE;
 	env->last_live_id = param;
 	env->nb_live++;
 	return (0);
