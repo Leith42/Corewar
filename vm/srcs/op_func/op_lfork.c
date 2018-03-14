d87b7b87dd98b3168708f69dfff361c8db833669
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-14 22:52:15 +0100

    Fixed bad opcode reference in lfork()

--- vm/srcs/op_func/op_lfork.c
+++ vm/srcs/op_func/op_lfork.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:54:41 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/14 22:43:03 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/14 22:51:16 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	do_lfork(t_process *process, t_env *env)
 	unsigned int	param;
 	t_process		*fork;
 
-	param = get_param_raw_value(env, process->pc + 1, T_DIR, OP_FORK);
+	param = get_param_raw_value(env, process->pc + 1, T_DIR, OP_LFORK);
 	add_new_process(env, process->champ_id);
 	fork = (t_process *)env->process->content;
 	memcpy(fork, process, sizeof(t_process));
