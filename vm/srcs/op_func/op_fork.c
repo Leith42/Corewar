9691c6770ef4af78dad489a8ba3c7710e8e9e869
Mathieu GONON <mgonon@student.42.fr>
2018-03-14 18:57:15 +0100

    fix op_fork

--- vm/srcs/op_func/op_fork.c
+++ vm/srcs/op_func/op_fork.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:53:50 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/14 00:35:49 by mgonon           ###   ########.fr       */
+/*   Updated: 2018/03/14 18:55:27 by mgonon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	do_fork(t_process *process, t_env *env)
 	t_process		*fork;
 
 	param = (short)get_param_raw_value(env, process->pc + 1, T_DIR, OP_FORK);
-	fork = add_new_process(env, process->champ_id);
+	add_new_process(env, process->champ_id);
+	fork = (t_process *)env->process->content;
 	memcpy(fork, process, sizeof(t_process));
 	fork->pc = process->pc + (param % IDX_MOD);
 	return (0);
