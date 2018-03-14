fb1a9dc85849df17494c50f9a7e6df9e3b3030b4
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-14 18:37:09 +0100

    do_live now sets env->last_live_id only on valid ID.

--- vm/srcs/op_func/op_live.c
+++ vm/srcs/op_func/op_live.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:55:03 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/13 23:10:04 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/14 18:36:16 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,12 @@ int		do_live(t_process *process, t_env *env)
 
 	param = get_param_raw_value(env, process->pc + 1, T_DIR, OP_LIVE);
 	if ((champion_name = get_champ_name(env, param)))
+	{
+		env->last_live_id = param;
 		ft_printf("A process report that the champion %s (%u) is alive.\n",
 					champion_name, param);
+	}
 	process->is_alive = true;
-	env->last_live_id = param;
 	env->nb_live++;
 	process->pc += 1 + 4;
 	return (0);
