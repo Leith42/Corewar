4445c33bb051de88922b748febf82b9e884271bb
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-14 20:54:20 +0100

    Corrected op_sti(). Now functionnal.

--- vm/srcs/op_func/op_sti.c
+++ vm/srcs/op_func/op_sti.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:53:40 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/14 20:22:37 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/14 20:53:28 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@ int	do_sti(t_process *process, t_env *env)
 	type_3 = get_param_type(env, process->pc, OP_STI, 2);
 	val_1 = get_param_raw_value(env, process->pc + 2, T_REG, OP_STI);
 	val_2 = get_param_raw_value(env, process->pc + 3, type_2, OP_STI);
-	if (get_param_type(env, process->pc, OP_STI, 2) == T_REG
-		&& type_2 & g_op_tab[OP_STI - 1].param_type[1]
-		&& type_3 & g_op_tab[OP_STI - 1].param_type[2]
+	if (get_param_type(env, process->pc, OP_STI, 0) == T_REG
+		&& (type_2 & g_op_tab[OP_STI - 1].param_type[1]) != 0
+		&& (type_3 & g_op_tab[OP_STI - 1].param_type[2]) != 0
 		&& val_1 > 0 && val_1 <= REG_NUMBER
 		&& (type_2 != T_REG || (val_2 > 0 && val_2 <= REG_NUMBER)))
 	{
+		val_1 = process->reg[val_1 - 1];
 		val_3 = get_param_raw_value(env,
 				process->pc + 3 + ((type_2 == T_REG) ? 1 : 2),
 				type_3, OP_STI);
