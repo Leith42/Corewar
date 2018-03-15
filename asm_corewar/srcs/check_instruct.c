4f4bbf3ad652e5a76e082b340da54b6e55faf5a2
Lachlan GRAHAM <lgraham@e1r6p2.42.fr>
2018-03-15 08:35:05 +0100

    first draft at finding and checking labels; lines added in check_instruct have been put as comments.

--- asm_corewar/srcs/check_instruct.c
+++ asm_corewar/srcs/check_instruct.c
@@ -6,7 +6,7 @@
 /*   By: lmartin- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 16:54:16 by lmartin-          #+#    #+#             */
-/*   Updated: 2018/03/09 16:54:18 by lmartin-         ###   ########.fr       */
+/*   Updated: 2018/03/15 08:32:22 by lgraham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ int					get_inst(char **inst)
 
 	i = 0;
 	n = (inst[0][ft_strlen(inst[0]) - 1] == ':') ? 1 : 0;
+	ft_check_label(inst, tab);
 	/*
 	** Le cas du label en debut de chaine n'est pas gere
 	*/
@@ -79,16 +80,20 @@ int					check_inst(char *line, unsigned char **tab)
 {
 	char	*line;
 	char	**inst;
+//	t_label	*label;
 
 	while (get_next_line(fd, &line) > 0, 16)
 	{
 		if (ft_split_inst(line))
 		{
+//			ft_check_label(inst, label); //stock labels in lists.
 			if (!(get_inst(inst)))
 				return (0); //Instruction incorrecte
 			ft_free_arr(inst);
 		}
 		free(line);
 	}
+//	if (ft_double_check_label(label) == -1) //checks label calls for adequate declaration.
+//		return (0);
 	return (1);
 }
