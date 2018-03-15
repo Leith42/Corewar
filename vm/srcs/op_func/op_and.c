6bfc5e01261d842078d382bb4c612e0a92a48872
Amine <aazri@student.42.fr>
2018-03-15 12:58:06 +0100

    op_and first ver.

--- vm/srcs/op_func/op_and.c
+++ vm/srcs/op_func/op_and.c
@@ -12,9 +12,30 @@
 
 #include "vm.h"
 
-int	do_and(t_process *process, t_env *env)
+int			do_and(t_process *process, t_env *env)
 {
-	(void)process;
-	(void)env;
+	unsigned short	param_type[3];
+	unsigned int	param_value[3];
+	unsigned int	skip;
+
+	param_type[0] = get_param_type(env, process->pc, OP_AND, 0);
+	param_type[1] = get_param_type(env, process->pc, OP_AND, 1);
+	param_type[2] = get_param_type(env, process->pc, OP_AND, 2);
+	if (param_type[2] == T_REG)
+	{
+		param_value[0] = get_param_raw_value(
+				env, process->pc + 2, param_type[0], OP_AND);
+		skip = (param_type[0] == T_DIR || param_type[0] == T_IND) ? 5 : 3;
+		param_value[1] = get_param_raw_value(
+				env, process->pc + skip, param_type[1], OP_AND);
+		skip += (param_type[1] == T_DIR || param_type[1] == T_IND) ? 4 : 2;
+		param_value[2] = get_param_raw_value(
+				env, process->pc + skip, param_type[2], OP_AND);
+		if (is_reg(param_value[2]))
+		{
+			process->reg[param_value[2] - 1] = param_value[0] & param_value[1];
+			return (process->reg[param_value[2] - 1] == 0);
+		}
+	}
 	return (0);
 }
