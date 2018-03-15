26135415a74bc0725dce575622813adc1131795b
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-15 22:03:26 +0100

    Norm on op_sti()

--- vm/srcs/op_func/op_sti.c
+++ vm/srcs/op_func/op_sti.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:53:40 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/15 18:03:23 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/15 22:02:43 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,27 @@
 ** T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG
 */
 
-int	do_sti(t_process *process, t_env *env)
+static void		do_sti_bis(t_env *env, t_process *process,
+				unsigned short *type, unsigned int *val)
+{
+	unsigned int	start;
+
+	if (type[1] != T_DIR)
+		val[1] = (type[1] == T_REG) ? process->reg[val[1] - 1]
+			: get_uintfrom_char(env, (process->pc +
+			(((short)(val[1])) % IDX_MOD)) % MEM_SIZE, 4);
+	write_uint_to_char(env, start = ((process->pc +
+			(((short)(val[1] + val[2])) % IDX_MOD)) % MEM_SIZE), 4, val[0]);
+	env->mask[(start + 0) % MEM_SIZE] = process->champ_id;
+	env->mask[(start + 1) % MEM_SIZE] = process->champ_id;
+	env->mask[(start + 2) % MEM_SIZE] = process->champ_id;
+	env->mask[(start + 3) % MEM_SIZE] = process->champ_id;
+}
+
+int				do_sti(t_process *process, t_env *env)
 {
 	unsigned short	type[3];
 	unsigned int	val[3];
-	unsigned int	write_pc;
 
 	type[1] = get_param_type(env, process->pc, OP_STI, 1);
 	type[2] = get_param_type(env, process->pc, OP_STI, 2);
@@ -37,15 +53,6 @@ int	do_sti(t_process *process, t_env *env)
 	if (!(type[2] != T_REG || is_reg(val[2])))
 		return (0);
 	val[2] = (type[2] == T_REG) ? process->reg[val[2] - 1] : val[2];
-	if (type[1] != T_DIR)
-		val[1] = (type[1] == T_REG) ? process->reg[val[1] - 1]
-			: get_uintfrom_char(env, (process->pc +
-			(((short)(val[1])) % IDX_MOD)) % MEM_SIZE, 4);
-	write_uint_to_char(env, write_pc = ((process->pc + (((short)(val[1] + val[2]))
-		% IDX_MOD)) % MEM_SIZE), 4, val[0]);
-	env->mask[(write_pc + 0) % MEM_SIZE] = process->champ_id;
-	env->mask[(write_pc + 1) % MEM_SIZE] = process->champ_id;
-	env->mask[(write_pc + 2) % MEM_SIZE] = process->champ_id;
-	env->mask[(write_pc + 3) % MEM_SIZE] = process->champ_id;
+	do_sti_bis(env, process, type, val);
 	return (0);
 }
