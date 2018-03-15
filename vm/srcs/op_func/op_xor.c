a9ed24c206190e3f0cef02a4ae8c096dfd36b455
Amine <aazri@student.42.fr>
2018-03-15 13:02:52 +0100

    op_xor first ver.

--- vm/srcs/op_func/op_xor.c
+++ vm/srcs/op_func/op_xor.c
@@ -14,7 +14,28 @@
 
 int	do_xor(t_process *process, t_env *env)
 {
-	(void)process;
-	(void)env;
+	unsigned short	param_type[3];
+	unsigned int	param_value[3];
+	unsigned int	skip;
+
+	param_type[0] = get_param_type(env, process->pc, OP_XOR, 0);
+	param_type[1] = get_param_type(env, process->pc, OP_XOR, 1);
+	param_type[2] = get_param_type(env, process->pc, OP_XOR, 2);
+	if (param_type[2] == T_REG)
+	{
+		param_value[0] = get_param_raw_value(
+				env, process->pc + 2, param_type[0], OP_XOR);
+		skip = (param_type[0] == T_DIR || param_type[0] == T_IND) ? 5 : 3;
+		param_value[1] = get_param_raw_value(
+				env, process->pc + skip, param_type[1], OP_XOR);
+		skip += (param_type[1] == T_DIR || param_type[1] == T_IND) ? 4 : 2;
+		param_value[2] = get_param_raw_value(
+				env, process->pc + skip, param_type[2], OP_XOR);
+		if (is_reg(param_value[2]))
+		{
+			process->reg[param_value[2] - 1] = param_value[0] ^ param_value[1];
+			return (process->reg[param_value[2] - 1] == 0);
+		}
+	}
 	return (0);
 }
