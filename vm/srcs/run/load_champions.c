04c70a584df052fb7e4ddc1e36e0a559014a91c5
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-16 23:03:27 +0100

    load_champions() now correctly iterates from the end of the list of champions. This fixes the bug of bad ID of initial processes compared to their champions.

--- vm/srcs/run/load_champions.c
+++ vm/srcs/run/load_champions.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 09:57:28 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/16 00:27:56 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/16 23:02:32 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,22 @@
 /*
 ** Loads champions in arena, with equally separated points of entry.
 ** Initializes mask ID values.
+** The get_next_proc_backwards is a bad hack needed because the process list
+** needs to be created backwards.
 */
+
+static t_process	*get_next_proc_backwards(t_env *env, unsigned int champ_nb)
+{
+	t_list	*ret;
+
+	ret = env->process;
+	while (champ_nb < env->nb_of_champions - 1)
+	{
+		ret = ret->next;
+		champ_nb++;
+	}
+	return ((t_process *)ret->content);
+}
 
 void				load_champions(t_env *env)
 {
@@ -23,18 +38,15 @@ void				load_champions(t_env *env)
 	unsigned int	entry_point;
 	unsigned int	end_point;
 	t_process		*process;
-	t_list			*lst_process;
 
 	entry_point = 0;
 	while (entry_point < MEM_SIZE)
 		env->mask[entry_point++] = NULL_ID;
 	champ_nb = 0;
-	lst_process = env->process;
 	while (champ_nb < env->nb_of_champions)
 	{
-		process = (t_process *)lst_process->content;
-		entry_point = ((MEM_SIZE / env->nb_of_champions) *
-				(env->nb_of_champions - champ_nb - 1));
+		process = get_next_proc_backwards(env, champ_nb);
+		entry_point = ((MEM_SIZE / env->nb_of_champions) * champ_nb);
 		process->pc = entry_point;
 		end_point = entry_point + env->champions[champ_nb].header.prog_size;
 		ft_memcpy(env->arena + entry_point,
@@ -43,6 +55,5 @@ void				load_champions(t_env *env)
 		while (entry_point < end_point)
 			env->mask[entry_point++] = env->champions[champ_nb].id;
 		champ_nb++;
-		lst_process = lst_process->next;
 	}
 }
