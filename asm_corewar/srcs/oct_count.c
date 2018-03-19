a3ef5e11cdfc725394b371301e71a1ae9736569e
Lachlan GRAHAM <lgraham@e1r10p22.42.fr>
2018-03-19 01:26:21 +0100

    ajout de commentaires qui se veulent explicatifs

--- asm_corewar/srcs/oct_count.c
+++ asm_corewar/srcs/oct_count.c
@@ -6,11 +6,18 @@
 /*   By: lgraham <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 03:49:24 by lgraham           #+#    #+#             */
-/*   Updated: 2018/03/19 01:10:04 by lgraham          ###   ########.fr       */
+/*   Updated: 2018/03/19 01:25:29 by lgraham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
+
+/*
+** label->oct_tmp sauve la valeur totale courante d'octet depuis le debut du fichier
+** dans la fonction add_to_lst() du check_label.c jusqu'au debut de la ligne lue par
+** gnl.
+** label->oct sauve la valeur octale finale de la position du label.
+*/
 
 int		oct_prec(int pos, char **inst, int nb)
 {
@@ -20,18 +27,14 @@ int		oct_prec(int pos, char **inst, int nb)
 	i = nb;
 	if (nb == 0)
 		return (0);
-	while (inst[i])
+	while (inst[i] && (type = param_type(inst[i])) >= 1)
 	{
-		if (i > nb)
-		{
-			type = param_type(inst[i]);
-			if (type == T_REG)
-				pos -= 1;
-			if (type == T_DIR)
-				pos -= 4;
-			if (type == T_IND)
-				pos -= 2;
-		}
+		if (type == T_REG && i > nb)
+			pos -= 1;
+		if (type == T_DIR && i > nb)
+			pos -= 4;
+		if (type == T_IND && i > nb)
+			pos -= 2;
 		i++;
 	}
 	return (pos);
