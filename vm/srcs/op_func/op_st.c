1dbb97b1d0dd049c0c10f12efda3a00b9b55e0c4
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-19 23:27:36 +0100

    Fixed op_st() now dereferencing first argument (T_REG)

--- vm/srcs/op_func/op_st.c
+++ vm/srcs/op_func/op_st.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:55:38 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/16 20:33:23 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/19 23:26:00 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@ int	do_st(t_process *process, t_env *env)
 	val[1] = get_param_raw_value(env, process->pc + 3, type_2, OP_ST);
 	if (get_param_type(env, process->pc, OP_ST, 0) != T_REG
 		|| get_param_type(env, process->pc, 0x00, 2) != 0
-		|| (type_2 != T_IND && type_2 != T_REG)
-		|| !is_reg(val[0])
+		|| (type_2 != T_IND && type_2 != T_REG) || !is_reg(val[0])
 		|| (type_2 == T_REG && !is_reg(val[1])))
 		return (0);
+	val[0] = process->reg[val[0] - 1];
 	if (type_2 == T_REG)
 		process->reg[val[1] - 1] = val[0];
 	else
 	{
 		write_uint_to_char(env, write_pc = (process->pc +
-				((short)val[1]) % IDX_MOD), REG_SIZE, val[0]);
+				((short)val[1]) % IDX_MOD) % MEM_SIZE, REG_SIZE, val[0]);
 		env->mask[(write_pc + 0) % MEM_SIZE] = process->champ_id;
 		env->mask[(write_pc + 1) % MEM_SIZE] = process->champ_id;
 		env->mask[(write_pc + 2) % MEM_SIZE] = process->champ_id;
