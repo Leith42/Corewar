cf5c8ef5d6c25c39753d149b89698d0ea366b7e5
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-19 17:24:44 +0100

    Fixed an pc overflow possibility in op_zjmp()

--- vm/srcs/op_func/op_zjmp.c
+++ vm/srcs/op_func/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:56:02 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/14 21:51:00 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/19 17:23:29 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int	do_zjmp(t_process *process, t_env *env)
 {
 	if (process->carry == 1)
+	{
 		process->pc += (((short)get_param_raw_value(env, process->pc + 1,
 			T_DIR, OP_ZJMP)) % IDX_MOD);
+		process->pc %= MEM_SIZE;
+	}
 	return (0);
 }
