c55df291452543fa67cfea115b9a145c79920182
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-23 18:54:53 +0100

    Small improvement on process data output

--- vm/srcs/run/utils2.c
+++ vm/srcs/run/utils2.c
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 17:59:15 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/23 18:41:27 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/23 18:51:33 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,24 @@ void			disp_process_state(t_env *env, t_process *proc)
 	size_t		i;
 
 	if (proc->cycle_to_wait != 0)
-		ft_printf("\x1b[KLe process du joueur %s%-4d\x1b[0m dort \
+		ft_printf("\x1b[KLe joueur %s%-4d\x1b[0m dort \
 durant \x1b[32m%-4d\x1b[0m cycles au pc \x1b[1;36m%-4d\x1b[0m ",
 		get_champ_color(env, proc->champ_id),
 		proc->champ_id, proc->cycle_to_wait, proc->pc);
 	else
-		ft_printf("\x1b[KLe process du joueur %s%-4d\x1b[0m \
+		ft_printf("\x1b[KLe joueur %s%-4d\x1b[0m \
 effectue un \x1b[1;32m%-4s (%#-.2hx)\x1b[0m au pc \x1b[1;36m%-4d\x1b[0m ",
 		get_champ_color(env, proc->champ_id), proc->champ_id,
 		(proc->cur_opcode > 16 || proc->cur_opcode == 0) ?
 		"null" : g_op_tab[proc->cur_opcode - 1].name,
 		(proc->cur_opcode > 16 || proc->cur_opcode == 0) ?
 		0x00 : g_op_tab[proc->cur_opcode - 1].opcode, proc->pc);
-	ft_putstr("reg = {");
+	ft_putstr("reg = \x1b[1m{\x1b[0m");
 	i = 0;
 	while (i < REG_NUMBER)
 	{
-		ft_printf("%s%x\x1b[0m%c", (proc->reg[i] == 0) ? "\x1b[2m" : "\x1b[0m",
-				proc->reg[i], (i == 15) ? '}' : ',');
+		ft_printf("%s%x%s", (proc->reg[i] == 0) ? "\x1b[2m" : "\x1b[0m",
+				proc->reg[i], (i == 15) ? "\x1b[0;1m}\x1b[0m" : "\x1b[0m,");
 		i++;
 	}
 	ft_putstr("\n\x1b[K");
