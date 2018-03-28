7611c2e280f9fd8e47cbdda46f35809c27c9e750
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-03-28 14:09:51 +0200

    Norm on op_and(), op_or(), op_xor()

--- vm/srcs/op_func/op_and.c
+++ vm/srcs/op_func/op_and.c
@@ -6,31 +6,31 @@
 /*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 23:51:50 by gudemare          #+#    #+#             */
-/*   Updated: 2018/03/27 18:58:24 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/03/28 14:04:59 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-static unsigned int	get_skip(unsigned short param_type)
+static unsigned int	get_skip(unsigned short type)
 {
-	if (param_type == T_REG)
+	if (type == T_REG)
 		return (1);
-	else if (param_type == T_IND)
+	else if (type == T_IND)
 		return (2);
-	else if (param_type == T_DIR)
+	else if (type == T_DIR)
 		return (4);
 	return (0);
 }
 
-static unsigned int	get_value(unsigned short param_type, unsigned int value,
+static unsigned int	get_value(unsigned short type, unsigned int value,
 					t_process *process, t_env *env)
 {
-	if (param_type == T_REG)
+	if (type == T_REG)
 		return (process->reg[value - 1]);
-	else if (param_type == T_DIR)
+	else if (type == T_DIR)
 		return (value);
-	else if (param_type == T_IND)
+	else if (type == T_IND)
 		return (get_uintfrom_char(env, (process->pc +
 			(((short)(value)) % IDX_MOD)) % MEM_SIZE, 4));
 	else
@@ -40,30 +40,26 @@ static unsigned int	get_value(unsigned short param_type, unsigned int value,
 int					do_and(t_process *process, t_env *env)
 {
 	unsigned int	skip;
-	unsigned short	param_type[3];
-	unsigned int	param_value[3];
+	unsigned short	type[3];
+	unsigned int	value[3];
 
 	skip = 2;
-	param_type[0] = get_param_type(env, process->pc, OP_AND, 0);
-	param_type[1] = get_param_type(env, process->pc, OP_AND, 1);
-	param_type[2] = get_param_type(env, process->pc, OP_AND, 2);
-	if (!(param_type[0] != 0 && param_type[1] != 0 && param_type[2] == T_REG))
+	type[0] = get_param_type(env, process->pc, OP_AND, 0);
+	type[1] = get_param_type(env, process->pc, OP_AND, 1);
+	type[2] = get_param_type(env, process->pc, OP_AND, 2);
+	if (!(type[0] != 0 && type[1] != 0 && type[2] == T_REG))
 		return (0);
-	param_value[0] = get_param_raw_value(env, process->pc + skip,
-			param_type[0], OP_AND);
-	skip += get_skip(param_type[0]);
-	param_value[1] = get_param_raw_value(env, process->pc + skip,
-			param_type[1], OP_AND);
-	skip += get_skip(param_type[1]);
-	param_value[2] = get_param_raw_value(
-			env, process->pc + skip, param_type[2], OP_AND);
-	if (!(is_reg(param_value[2])
-		&& (param_type[0] != T_REG || is_reg(param_value[0]))
-		&& (param_type[1] != T_REG || is_reg(param_value[1]))))
+	value[0] = get_param_raw_value(env, process->pc + skip, type[0], OP_AND);
+	skip += get_skip(type[0]);
+	value[1] = get_param_raw_value(env, process->pc + skip, type[1], OP_AND);
+	skip += get_skip(type[1]);
+	value[2] = get_param_raw_value(env, process->pc + skip, type[2], OP_AND);
+	if (!(is_reg(value[2])
+		&& (type[0] != T_REG || is_reg(value[0]))
+		&& (type[1] != T_REG || is_reg(value[1]))))
 		return (1);
-	param_value[0] = get_value(param_type[0], param_value[0], process, env);
-	param_value[1] = get_value(param_type[1], param_value[1], process, env);
-	process->reg[param_value[2] - 1] =
-		(param_value[0] & param_value[1]);
-	return ((process->reg[param_value[2] - 1] == 0) ? 0 : 1);
+	value[0] = get_value(type[0], value[0], process, env);
+	value[1] = get_value(type[1], value[1], process, env);
+	process->reg[value[2] - 1] = (value[0] & value[1]);
+	return ((process->reg[value[2] - 1] == 0) ? 0 : 1);
 }
