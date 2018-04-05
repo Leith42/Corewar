79cbc0d3ac2506bed52f005c0f00c4b6a24a7ff4
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-04-05 03:16:47 +0200

    Fixed potential bug where open() could be called on NULL argument.

--- asm_corewar/srcs/main.c
+++ asm_corewar/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lmartin- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/15 18:21:49 by lmartin-          #+#    #+#             */
-/*   Updated: 2018/03/22 10:07:06 by lgraham          ###   ########.fr       */
+/*   Updated: 2018/04/05 03:16:19 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ int		main(int ac, char **av)
 {
 	int fd;
 
-	fd = open(av[1], O_RDONLY);
 	if (ac != 2)
 		write(1, "Usage: ./asm File_Name\n", 23);
 	else if (!(ft_strnequ(av[1] + ft_strlen(av[1]) - 2, ".s", 2)))
 		write(1, "Wrong file\n", 11);
-	else if (fd < 0)
+	else if ((fd = open(av[1], O_RDONLY)) < 0)
 		write(1, "Wrong file\n", 11);
 	else
 		ft_read_file(fd, av[1]);
